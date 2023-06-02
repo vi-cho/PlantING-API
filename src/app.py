7179a5e270f986804748bdcf04adfa022aa97bdf
IGNACIO VITA <ivita@uc.cl>
2023-06-02 13:27:10 -0400

    update app.py

--- src/app.py
+++ src/app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello World!</p>'
+    texto = '<p>Bienvenido a PlantING!</p>\n<a href="/comenzar">Comenzar</a>'
+    return texto
+
+@app.route('/comenzar', methods=["GET", "POST"])
+def requirements_form():
+    return '<p>Aca se mostrara el formulario</p>'
